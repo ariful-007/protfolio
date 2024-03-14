1d16c3dea930f25e5ca98ee33821ea1fae2f84e9
ariful007 <arifulislamwd007@gmail.com>
2024-03-14 16:33:00 +0600

    google map update

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -11,7 +11,7 @@ const Contact = () => {
       <section className="mapbox" data-mapbox>
         <figure>
           <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d199666.5651251294!2d-121.58334177520186!3d38.56165006739519!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x809ac672b28397f9%3A0x921f6aaa74197fdb!2sSacramento%2C%20CA%2C%20USA!5e0!3m2!1sen!2sbd!4v1647608789441!5m2!1sen!2sbd"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3642.0788328674444!2d90.43358024456133!3d24.09872301990009!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755d9c567761413%3A0x91c88aa54b225f2c!2sRajendrapur%20Cantonment%2C%20Gazipur!5e0!3m2!1sen!2sbd!4v1710412018864!5m2!1sen!2sbd"
             width="400" height="300" loading="lazy" title="Google Map"></iframe>
         </figure>
       </section>
